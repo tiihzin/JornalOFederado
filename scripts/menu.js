4e7ebe0b66e928eb784bbdd2f2de3c12ad28ae62
João Tiago <tig.j2025@gmail.com>
2024-01-10 14:22:21 -0300

    .

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,5 +1,8 @@
 let menu = document.querySelector('#menu_mobile');
+let bg_menu = document.querySelector('#bg_menu');
 
 function openMenu() {
     menu.classList.toggle('menu_hiddem')
+    bg_menu.classList.toggle('menu_hiddem')
+    document.querySelector('#body').classList.toggle('static')
 }
